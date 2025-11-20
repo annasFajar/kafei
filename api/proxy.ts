3b7dc91a0013065eda931ff123e7e064e7aaecd3
annasfajarramadhan2@gmail.com <annasfajarramadhan2@gmail.com>
2025-11-20 22:24:39 +0700

    deploy-fix6

--- api/proxy.ts
+++ api/proxy.ts
@@ -11,11 +11,11 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     : "";
 
   // ambil query string setelah tanda ?
-  const qs = req.url!.split("?")[1] || "";
+  const qs = req.url!.split("?")[1] || ""
 
-  const targetURL = `https://wpu-cafe.vercel.app/api/${endpoint}${qs ? `?${qs}` : ""}`;
+  const targetURL = `https://wpu-cafe.vercel.app/api/${endpoint}${qs ? `?${qs}` : ""}`
 
-  console.log("Proxy →", targetURL);
+  console.log("Proxy →", targetURL)
 
   try {
     const response = await fetch(targetURL, {
@@ -25,22 +25,22 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
         "Content-Type": "application/json",
       },
       body: req.method !== "GET" ? JSON.stringify(req.body) : undefined,
-    });
+    })
 
     // ambil text dulu (karena bisa JSON atau HTML)
-    const text = await response.text();
+    const text = await response.text()
 
     // coba parse JSON
     try {
-      return res.status(response.status).json(JSON.parse(text));
+      return res.status(response.status).json(JSON.parse(text))
     } catch {
       // kalau bukan JSON, kirim raw HTML/teks biar tahu error backend
       return res.status(500).json({
         error: "Invalid JSON from backend",
         raw: text,
-      });
+      })
     }
   } catch (err: any) {
-    res.status(500).json({ error: "Proxy error", details: err.message });
+    res.status(500).json({ error: "Proxy error", details: err.message })
   }
 }
